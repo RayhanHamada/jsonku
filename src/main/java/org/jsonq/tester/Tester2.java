60048711488729c93b6a9b0a59f98d727d65de81
RayhanHamada <hamadadesk1999@gmail.com>
2019-04-14 17:54:00 +0700

    adding JSONFile class functionality and json dump

--- src/main/java/org/jsonq/tester/Tester2.java
+++ src/main/java/org/jsonq/tester/Tester2.java
@@ -1,21 +1,16 @@
 package org.jsonq.tester;
 
 import org.jsonq.core.extras.JSONFile;
-import org.jsonq.core.jsonvalue.JSONNumber;
-import org.jsonq.core.jsonvalue.JSONString;
 
 public class Tester2 {
 
     public static void main(String[] args) throws Exception {
 
         JSONFile file = new JSONFile("D:\\RayhanHamada\\CodingStuff\\Exercise\\Java\\jsonq\\src\\main\\resources\\j2.json");
-//        file.getJSONObject().getJSONArrayOf("nama").addString("wew");
         System.out.println(file.getJSONObject().getJSONArrayOf("nama").getStringAt(0).getLiteralValue());
         file.dump();
 
 
 
     }
-
-
 }
